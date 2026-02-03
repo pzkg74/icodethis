239d1a1a172fc34c53ad05dc84e0f25835050304
Sam <147330804+pzkg74@users.noreply.github.com>
2026-02-03 20:05:25 +0000

    make hands continuous not discrete

--- rand/analogue-clock/script.js
+++ rand/analogue-clock/script.js
@@ -2,11 +2,28 @@ const SECOND_IN_MS = 1000;
 
 const SECONDS_IN_MINUTE = 60;
 const MINUTES_IN_HOUR = 60;
+
+const SECONDS_IN_HOUR = 60 * 60;
 const HOURS_ON_CLOCK = 12;
 
 const secondHand = document.getElementById('seconds');
 const minuteHand = document.getElementById('minutes');
 const hourHand = document.getElementById('hours');
+
+function setHands() {
+  secondHand.style.setProperty(
+    '--rotation',
+    `${time.second / SECONDS_IN_MINUTE}turn`,
+  );
+  minuteHand.style.setProperty(
+    '--rotation',
+    `${(time.minute * SECONDS_IN_MINUTE + time.second) / SECONDS_IN_HOUR}turn`,
+  );
+  hourHand.style.setProperty(
+    '--rotation',
+    `${(time.hour * SECONDS_IN_HOUR + time.minute * SECONDS_IN_MINUTE + time.second) / (SECONDS_IN_HOUR * 12)}turn`,
+  );
+}
 
 const date = new Date();
 time = {
@@ -15,23 +32,19 @@ time = {
   second: date.getSeconds(),
 };
 
-secondHand.style.transform = `rotate(${time.second / SECONDS_IN_MINUTE}turn)`;
-minuteHand.style.transform = `rotate(${time.minute / MINUTES_IN_HOUR}turn)`;
-hourHand.style.transform = `rotate(${time.hour / HOURS_ON_CLOCK}turn)`;
+setHands();
 
 setInterval(() => {
   time.second += 1;
-  secondHand.style.transform = `rotate(${time.second / SECONDS_IN_MINUTE}turn)`;
+  setHands();
 
   if (time.second === SECONDS_IN_MINUTE) {
     time.second = 0;
     time.minute += 1;
-    minuteHand.style.transform = `rotate(${time.minute / MINUTES_IN_HOUR}turn)`;
 
     if (time.minute === MINUTES_IN_HOUR) {
       time.minute = 0;
       time.hour += 1;
-      hourHand.style.transform = `rotate(${(time.hour % 12) / HOURS_ON_CLOCK}turn)`;
     }
   }
 
